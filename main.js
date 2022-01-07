8f00b8d3ea8d7d5631dd9589a0f2df0621a4731f
dima-kudryk-frontnend <95941142+dima-kudryk-frontnend@users.noreply.github.com>
2022-01-07 22:31:50 +0200

    Together JS

---
+++ main.js
@@ -0,0 +1,10 @@
+let a = prompt(' Відкривай моє резюме, але перед цим помнож будь-які цифри')
+let n1 = +prompt("Введіть множник");
+let n2 = +prompt("Введіть множник");
+let result = 0
+let i = n1
+while (i <= n2) {
+    result = n1 * n2;
+    i++
+}
+alert(result)
