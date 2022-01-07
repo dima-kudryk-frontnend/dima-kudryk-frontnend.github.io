0ea52b0d18c8cfe705f248390bb14403d91c594d
dima-kudryk-frontnend <95941142+dima-kudryk-frontnend@users.noreply.github.com>
2022-01-07 22:56:44 +0200

    Finish this

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
-let a = prompt(' Відкривай моє резюме, але перед цим помнож будь-які цифри')
-let n1 = +prompt("Введіть множник");
-let n2 = +prompt("Введіть множник");
-let result = 0
-let i = n1
-while (i <= n2) {
-    result = n1 * n2;
-    i++
-}
-alert(result)
+let isBoss = confirm("Готовий ознайомитися з Моїм Резюме?");
+
+alert(isBoss);
